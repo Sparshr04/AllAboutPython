b3ed85704b848bdf82c2d2870dd3b9d505ae88e1
Sparsh Rannaware <rannawaresparsh@gmail.com>
2025-09-18 00:46:30 +0530

    Make Class for Snake Functions

--- Snake_Game/main.py
+++ Snake_Game/main.py
@@ -1,70 +1,26 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
-# tim = Turtle(shape='square')
-# tim.color('white')
-starting_position = [(0,0), (-20, 0), (-40, 0)]
-turtle_body = []
 
 screen = Screen()
 screen.bgcolor("black")
+screen.setup(width=600, height=600)
 screen.title("Snake Game")
 screen.tracer(0)
 
-# for turtle in range(0,3):
-#     new_part = Turtle(shape='square')
-#     new_part.color('white')
-#     turtle_body.append(new_part)
-for position in starting_position:
-    new_segment = Turtle(shape='square')
-    new_segment.color('white')
-    new_segment.pu()
-    new_segment.goto(position)
-    turtle_body.append(new_segment)
+snake = Snake()
 
+screen.listen()
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    # for seg in turtle_body:
-    #     seg.forward(20)
-    for seg in range(len(turtle_body)-1, 0, -1):
-        new_x = turtle_body[seg-1].xcor()
-        new_y = turtle_body[seg-1].ycor()
-        turtle_body[seg].goto(new_x, new_y)
 
-    turtle_body[0].forward(20)
-    turtle_body[0].right(90)
+    snake.move()
 
-# def forward():
-#     turtle_body[0].forward(30)
-#     position = turtle_body[0].pos()
-#     turtle_body[1].goto(-21 + position[0],0 + position[1])
-#     turtle_body[2].goto(-41 + position[0],0 + position[1])
-    
-
-# def left():
-#     turtle_body[0].left(90)
-#     position = turtle_body[0].pos()
-#     turtle_body[1].goto(-21 + position[0],0 + position[1])
-#     turtle_body[2].goto(-41 + position[0],0 + position[1])
-
-# def right():
-#     turtle_body[0].right(90)
-#     position = turtle_body[0].pos()
-#     turtle_body[1].goto(-21 + position[0],0 + position[1])
-#     turtle_body[2].goto(-41 + position[0],0 + position[1])
-
-# ________________Initial Trial code set__________________________
-# position = turtle_body[0].pos()
-# turtle_body[1].goto(-21 + position[0],0 + position[1])
-# turtle_body[2].goto(-41 + position[0],0 + position[1])
-# turtle_body[3].goto(-21,0)
-# _________________________________________________________________
-
-
-# screen.listen()
-# screen.onkey(key='w', fun=forward)
-# screen.onkey(key='a', fun=left)
-# screen.onkey(key='d', fun=right)
 screen.exitonclick()

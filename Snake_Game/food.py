f1110201e77fa8b8276ba113e3afd05eb12798e1
Sparsh Rannaware <rannawaresparsh@gmail.com>
2025-09-19 10:59:28 +0530

    Create Food and Scorefoard class

---
+++ Snake_Game/food.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+    def __init__(self, shape = "classic", undobuffersize = 1000, visible = True):
+        super().__init__(shape, undobuffersize, visible)
+        self.shape("circle")
+        self.pu()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("red")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
---
+++ Snake_Game/scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('Arial', 15, 'normal')
+
+class Score(Turtle):
+    def __init__(self, shape = "classic", undobuffersize = 1000, visible = True):
+        super().__init__(shape, undobuffersize, visible)
+        self.color("white")
+        self.pu()
+        self.speed("fastest")
+        self.hideturtle()
+        self.goto(-10, 275)
+        self.count = 0
+        self.score_update()
+
+    def collusion(self):
+        self.count += 1
+        self.clear()
+        self.score_update()
+
+    def score_update(self):
+        self.write(f'Score: {self.count}', move=False, align=ALIGNMENT, font=FONT)
+        
+        
+
